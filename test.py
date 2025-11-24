b2530cfddfea701e21af5789e51e1ba9e06cbf68
sujal2005code <143809728+sujal2005code@users.noreply.github.com>
2025-11-24 23:24:44 +0530

    Update test.py auto testing

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 print ("mehak Pari hee kehde hai...")
 
-name = "bhola"
-age = 67
-new_age = 69
+name = "Maccua"
+age = 18
 
-print (f"My name is {name} and im {new_age} old...")
+
+print (f"My name is {name} and im {age} old...")
+
