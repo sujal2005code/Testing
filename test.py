73eabdf1c51fc97389e8a2b2a42a83b1559e8f84
sujal2005code <143809728+sujal2005code@users.noreply.github.com>
2025-11-24 23:19:12 +0530

    Update test.py new_age

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ print ("mehak Pari hee kehde hai...")
 
 name = "bhola"
 age = 67
+new_age = 69
 
-print (f"My name is {name} and im {age} old...")
+print (f"My name is {name} and im {new_age} old...")
